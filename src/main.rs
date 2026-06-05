492c0816ba9c3aa452b616d52692533dc98d1d37
Pedro Leal <git@pedroleal.pt>
2026-06-05 01:42:24 +0100

    Added crud WAL file inspection and CRC check

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,36 @@ use onyxdb::wal::WalRecord;
 
 fn main() {
 
-    let mut memtable = memtable::MemTable::new();
-
-    memtable.put(b"Key", b"value");
-
-    let read  = memtable.get(b"Key");
-
-    match read {
-        Some(val) => {
-            println!("{}", str::from_utf8(val).unwrap());
-        }
-        None => {
-            println!("No value");
-        }
-    }
+    // let mut memtable = memtable::MemTable::new();
+    //
+    // memtable.put(b"Key", b"value");
+    //
+    // let read  = memtable.get(b"Key");
+    //
+    // match read {
+    //     Some(val) => {
+    //         println!("{}", str::from_utf8(val).unwrap());
+    //     }
+    //     None => {
+    //         println!("No value");
+    //     }
+    // }
 
     let mut wal = wal::Wal::new();
     wal.init().unwrap();
 
-    let temp_rec = wal::WalRecord::new(b"mykey", b"bigbigvalue", 1, 0);
+    // for idx in 0..10 {
+    //     let temp_rec = WalRecord::new(b"DEADBEEF", b"XXXX", 1, idx);
+    //     match wal.append(&temp_rec) {
+    //         Ok(()) => {
+    //             println!("WAL append successful");
+    //         }
+    //         Err(e) => {
+    //             println!("{}", e)
+    //         }
+    //     }
+    //
+    // }
 
-    match wal.append(&temp_rec) {
-        Ok(()) => {
-            println!("WAL append successful");
-        }
-        Err(e) => {
-            println!("{}", e)
-        }
-    }
+    wal.inspect().unwrap();
 }
--- src/wal.rs
+++ src/wal.rs
@@ -1,5 +1,5 @@
-use std::{fs, io};
-use std::io::Write;
+use std::{fmt, fs, io};
+use std::io::{Read, Write};
 
 const WAL_FILE_PATH: &str = "./odb.wal";
 
@@ -36,25 +36,26 @@ impl WalRecord {
         }
     }
 
-    pub fn pack(&self) -> Vec<u8> {
-        let key_len = (self.key.len() as u32).to_le_bytes();
-        let val_len = (self.val.len() as u32).to_le_bytes();
-        let total_len =
+    pub fn header_size() -> usize {
             size_of::<u32>() +  // CRC32
             size_of::<u8>() +   // record_type
             size_of::<u64>() +  // batch_id
             size_of::<u64>() +  // seq_no
             size_of::<u32>() +  // key_len (le)
-            size_of::<u32>() +  // val_len (le)
-            self.key.len() +    // key
-            self.val.len();     // val
+            size_of::<u32>()  // val_len (le)
+    }
+
+    pub fn pack(&self) -> Vec<u8> {
+        let key_len = (self.key.len() as u32).to_le_bytes();
+        let val_len = (self.val.len() as u32).to_le_bytes();
+        let total_len = WalRecord::header_size() + self.key.len() +  self.val.len();
 
         let mut buf = Vec::with_capacity(total_len);
 
         buf.extend_from_slice(&[0,0,0,0]);                 // CRC32 placeholder
         buf.push(self.record_type);                              // record type
         buf.extend_from_slice(&vec![0; size_of::<u64>()]); // 0x0 batch_id
-        buf.extend_from_slice(&vec![0; size_of::<u64>()]); // 0x0 seq_no
+        buf.extend_from_slice(&self.seq_no.to_le_bytes());       // 0x0 seq_no
         buf.extend_from_slice(&key_len[..]);               // key length
         buf.extend_from_slice(&val_len[..]);               // val length
         buf.extend_from_slice(&self.key);                        // key
@@ -78,6 +79,8 @@ impl Wal {
     pub fn init(&mut self) -> Result<(), std::io::Error> {
         let mut open_options = fs::OpenOptions::new();
         open_options
+            .read(true)
+            .write(true)
             .append(true)
             .create(true);
 
@@ -99,6 +102,79 @@ impl Wal {
 
         file.write_all(&buf)?;
         file.sync_data()?;
+
+        Ok(())
+    }
+
+    pub fn inspect(&mut self) -> Result<(), io::Error> {
+        let file = self.curr_file.as_mut().ok_or_else(|| {
+            io::Error::new(io::ErrorKind::NotConnected, "WAL file not initialized")
+        })?;
+
+        let mut buf = vec![];
+        let result = file.read_to_end(&mut buf);
+        println!("read:{}", result.unwrap());
+
+        let mut base_offset = 0;
+        while base_offset < buf.len() {
+            // =HEADER
+            //  size_of::<u32>()  // CRC32
+            //  size_of::<u8>()   // record_type
+            //  size_of::<u64>()  // batch_id
+            //  size_of::<u64>()  // seq_no
+            //  size_of::<u32>()  // key_len (le)
+            //  size_of::<u32>()  // val_len (le)
+            // =BODY
+
+            // Header Offsets
+            let crc32_offset = base_offset + 0;
+            let record_type_offset = crc32_offset + size_of::<u32>();
+            let batch_id_offset = record_type_offset + size_of::<u8>();
+            let seq_no_offset = batch_id_offset + size_of::<u64>();
+            let key_len_offset = seq_no_offset + size_of::<u64>();
+            let val_len_offset = key_len_offset + size_of::<u32>();
+
+            // Header Slices
+            let crc32_slice:  [u8; 4]   = buf[crc32_offset..crc32_offset + size_of::<u32>()].try_into().unwrap();
+            let batch_id_slice: [u8; 8] = buf[batch_id_offset..batch_id_offset + size_of::<u64>()].try_into().unwrap();
+            let seq_no_slice: [u8; 8]   = buf[seq_no_offset..seq_no_offset + size_of::<u64>()].try_into().unwrap();
+            let kl_slice: [u8; 4]       = buf[key_len_offset..key_len_offset + size_of::<u32>()].try_into().unwrap();
+            let vl_slice: [u8; 4]       = buf[val_len_offset..val_len_offset + size_of::<u32>()].try_into().unwrap();
+
+            // Header Values
+            let record_crc32 = u32::from_le_bytes(crc32_slice);
+            let record_type   = buf[record_type_offset];
+            let batch_id = u64::from_le_bytes(batch_id_slice);
+            let seq_no = u64::from_le_bytes(seq_no_slice);
+            let kl = u32::from_le_bytes(kl_slice);
+            let vl = u32::from_le_bytes(vl_slice);
+
+            // Body Offsets
+            let key_offset = val_len_offset + size_of::<u32>();
+            let val_offset = key_offset + kl as usize;
+
+            // Body Values
+            let key: Vec<u8> = buf[key_offset..key_offset+kl as usize].into();
+            let val: Vec<u8> = buf[val_offset..val_offset+vl as usize].into();
+
+            let record_buf = &buf[base_offset + 4..val_offset+vl as usize];
+            let calc_crc32 = crc32fast::hash(record_buf);
+
+            println!("h[crc:{},crc_check:{},t:{},b:{},seq:{},kl:{},vl:{}]\tb[k:{},v:{}]",
+                     record_crc32,
+                     calc_crc32 == record_crc32,
+                     record_type,
+                     batch_id,
+                     seq_no,
+                     kl,
+                     vl,
+                     String::from_utf8(key).unwrap(),
+                     String::from_utf8(val).unwrap(),
+
+            );
+
+            base_offset = val_offset + vl as usize;
+        }
 
         Ok(())
     }
